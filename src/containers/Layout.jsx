dc86d6cc44d3fc3362a19a2a7a035192ed5f88c7
SergioCamiloCastillo <scastillonu@uniminuto.edu.co>
2021-10-06 22:38:57 -0500

    agregando paginas

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Header from '../components/Header';
 
-export function Layout({ children }) {
-    return (
-        <div className='layout'>
-            {children}
-        </div>
-    )
+const Layout = ({ children }) => {
+	return (
+		<div className="Layout">
+			<Header />
+			{children}
+		</div>
+	);
 }
+
+export {Layout};
